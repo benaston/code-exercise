e725535397f57ee7f488f2c3796468db6e9291b9
unknown <andols@QHSW20699.b2b.regn.net>
2013-02-14 03:33:38 +0000

    correction

--- Tesco.Code.Tests.Unit/LogAverageExecutionTimeDecoratorForTest.cs
+++ Tesco.Code.Tests.Unit/LogAverageExecutionTimeDecoratorForTest.cs
@@ -1,14 +1,16 @@
 namespace Tesco.Code.Tests.Unit
 {
-    public class LogAverageExecutionTimeDecoratorForTest<TDecoratee> : LogAverageExecutionTimeDecorator<TDecoratee> where TDecoratee : IAuthorisationService
+    public class LogAverageExecutionTimeDecoratorForTest<TDecoratee> : LogAverageExecutionTimeDecorator<TDecoratee>
+        where TDecoratee : IAuthorisationService
     {
-        public LogAverageExecutionTimeDecoratorForTest(TDecoratee decoratee, ITescoStopwatch stopwatch, ILogger logger, IClock clock, int sampleSize = 100) : base(decoratee, stopwatch, logger, clock, sampleSize)
+        public LogAverageExecutionTimeDecoratorForTest(TDecoratee decoratee, 
+            ITescoStopwatch stopwatch, ILogger logger, IClock clock, int sampleSize = 100)
+            : base(decoratee, stopwatch, logger, clock, sampleSize)
         {
         }
 
         /// <summary>
         /// Needed to ensure tests isolation with a static variable.
-        /// Would prefer this was not on the real concrete implementation like this.
         /// </summary>
         public static void ResetRequestCounter()
         {
